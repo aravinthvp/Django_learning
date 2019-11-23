44f5b79d99340fb92b07b22928a9e915919c78f5
vengatesharavinth_Pothiyappan <34703046+aravinthvp@users.noreply.github.com>
2019-11-23 23:16:09 +0530

    Add files via upload

--- firstproject/firstapp/admin.py
+++ firstproject/firstapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from firstapp.models import Webpage,Topic,AccessRecord
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
